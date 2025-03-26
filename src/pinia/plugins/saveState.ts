8b0876ef570759e895bf21bba89b90147019c284
dengshaomin86 <dengshaomin86@163.com>
2025-03-26 23:23:25 +0800

    piniaPluginSaveState

--- src/pinia/plugins/saveState.ts
+++ src/pinia/plugins/saveState.ts
@@ -1,12 +1,11 @@
-import fs from "fs";
-import path from "path";
-import { ipcRenderer } from "electron";
-import { cloneDeep } from "lodash";
-import { ref } from "vue";
-import type { PiniaPluginContext, Store } from "pinia";
-import { CACHES_DIR } from "@/utils/constants";
+import fs from 'fs';
+import path from 'path';
+import { ipcRenderer } from 'electron';
+import { cloneDeep } from 'lodash';
+import type { PiniaPluginContext, Store } from 'pinia';
+import { CACHES_DIR } from '@/utils/constants';
 
-const STORE_DIR = path.join(CACHES_DIR, "store");
+const STORE_DIR = path.join(CACHES_DIR, 'store');
 
 const handleFilename = (id: string) => `${id}.as`;
 
@@ -20,7 +19,7 @@ const ensureDirSync = (dest: string) => {
 function mergeState(target: any, source: any) {
   for (const key in source) {
     if (!(key in target)) continue; // target 不存在 key，直接跳过
-    target[key].toString() === "[object Object]" && source[key].toString() === "[object Object]"
+    target[key].toString() === '[object Object]' && source[key].toString() === '[object Object]'
       ? mergeState(target[key], source[key])
       : (target[key] = source[key]);
   }
@@ -32,13 +31,12 @@ function initState(store: Store, persist = false) {
   if (!persist) return;
   const _dir = path.join(STORE_DIR, handleFilename($id));
   if (!fs.existsSync(_dir)) return;
-  const content = fs.readFileSync(_dir, "utf8");
+  const content = fs.readFileSync(_dir, 'utf8');
   if (!content) return;
-  const state = mergeState(cloneDeep(store.$state), JSON.parse(content)) as { [key: string]: any };
-  for (const key in state) {
-    // @ts-ignore
-    store.$state[key] = ref(state[key]);
-  }
+  const initialState = mergeState(cloneDeep(store.$state), JSON.parse(content));
+  store.$patch((state: any) => {
+    Object.assign(state, initialState);
+  });
 }
 
 function writeState(store: Store, persist = false) {
@@ -46,14 +44,14 @@ function writeState(store: Store, persist = false) {
   if (!persist) return;
   const dest = path.join(STORE_DIR, handleFilename($id));
   ensureDirSync(dest);
-  fs.writeFileSync(dest, JSON.stringify(store.$state), "utf8");
+  fs.writeFileSync(dest, JSON.stringify(store.$state), 'utf8');
 }
 
 export function piniaPluginSaveState(context: PiniaPluginContext) {
   const { store, options } = context;
   const { persist } = options;
 
-  ipcRenderer.on("updateStoreState", () => initState(store, persist));
+  ipcRenderer.on('updateStoreState', () => initState(store, persist));
 
   initState(store, persist);
 
